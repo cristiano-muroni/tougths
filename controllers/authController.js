fa4c5d582a4b580be9917a89166b85db9b45d2c6
Cristiano da Silva Muroni <cristiano.muroni@grupoboticario.com.br>
2023-01-04 22:47:11 -0300

    criando rotas para registros, colocando flash messages no front

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,23 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 module.exports = class AuthController {
     static login(req, res) {
         res.render('auth/login');
-    }
+    };
 
     static register(req, res) {
         res.render('auth/register');
+    };
+
+    static async registerPost(req, res) {
+        const { name, email, password, confirmpassword } = req.body;
+        if(password !== confirmpassword){
+            req.flash('message', 'As senhas não conferem, tente novamente!');
+            res.render("auth/register");
+        }
+
     }
+
+
 
 }
