322c508b9692838bdd27bf20286bcb65e54f973c
Cristiano da Silva Muroni <cristiano.muroni@grupoboticario.com.br>
2023-01-05 22:30:23 -0300

    registrando usuário e criando a sessão

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,51 @@
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 module.exports = class AuthController {
-    static login(req, res) {
-        res.render('auth/login');
-    };
+  static login(req, res) {
+    res.render("auth/login");
+  }
 
-    static register(req, res) {
-        res.render('auth/register');
-    };
+  static register(req, res) {
+    res.render("auth/register");
+  }
 
-    static async registerPost(req, res) {
-        const { name, email, password, confirmpassword } = req.body;
-        if(password !== confirmpassword){
-            req.flash('message', 'As senhas não conferem, tente novamente!');
-            res.render("auth/register");
-        }
-
+  static async registerPost(req, res) {
+    const { name, email, password, confirmpassword } = req.body;
+    if (password !== confirmpassword) {
+      req.flash("message", "As senhas não conferem, tente novamente!");
+      res.render("auth/register");
+      return;
     }
 
+    const checkIfUserExists = await User.findOne({ where: { email: email } });
+    if (checkIfUserExists) {
+      req.flash("message", "O email já está em uso!");
+      res.render("auth/register");
+      return;
+    }
 
+    //create password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
 
-}
+    try {
+      const createdUser = await User.create(user);
+      console.log(user)
+
+      //initialize session
+      req.session.userid = createdUser.id;
+      req.flash("message", "Cadastro realizado com sucesso!");
+      req.session.save(() => {
+        res.redirect("/");
+      })      
+     
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
