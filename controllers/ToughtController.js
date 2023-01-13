1dfa2e9864fcfaff62acf3ac1d0b87d69f2bb33f
Cristiano Muroni <muronislick@hotmail.com>
2023-01-12 22:17:47 -0300

    Subindo serviços relacionados a exibição de toughts

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,13 +1,44 @@
-const Tought = require('../models/Tought');
-const User = require('../models/User');
+const Tought = require("../models/Tought");
+const User = require("../models/User");
 
 module.exports = class ToughtController {
-    static async showToughts(req, res) {
-        res.render('toughts/home');
-    }
+  static async showToughts(req, res) {
+    res.render("toughts/home");
+  }
 
-    static async dashboard(req, res) {
-        res.render('toughts/dashboard');
+  static async dashboard(req, res) {
+    const UserId = req.session.userid;
+    const user = await User.findOne({
+      where: { id: UserId },
+      include: Tought,
+      plain: true,
+    });
 
+    if (!user) {
+      res.redirect("/login");
     }
-}
+    const toughts = user.Toughts.map(result => result.dataValues);
+    console.log(toughts)
+    res.render("toughts/dashboard", {toughts});
+  };
+
+  static createTought(req, res) {
+    res.render("toughts/create");
+  }
+
+  static async createToughtSave(req, res) {
+    try {
+      const tought = {
+        title: req.body.title,
+        UserId: req.session.userid,
+      };
+      await Tought.create(tought);
+      req.flash("message", "pensamento criado com sucesso!");
+      req.session.save(() => {
+        res.redirect("/toughts/dashboard");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -3,6 +3,8 @@ const router = expres.Router();
 const ToughtController = require('../controllers/ToughtController');
 const checkAuth =  require('../helpers/auth').checkAuth;
 
+router.get('/add', checkAuth, ToughtController.createTought);
+router.post('/add', checkAuth, ToughtController.createToughtSave);
 router.get('/dashboard', checkAuth, ToughtController.dashboard);
 router.get('/', ToughtController.showToughts);
 
