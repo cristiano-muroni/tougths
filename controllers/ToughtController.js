5bf5a3d59c5d2b6ead641e2e9c1f168020b5f9bf
Cristiano Muroni <muronislick@hotmail.com>
2023-01-16 21:56:56 -0300

    routes, views e controllers de exibição da home

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -2,8 +2,13 @@ const Tought = require("../models/Tought");
 const User = require("../models/User");
 
 module.exports = class ToughtController {
+
   static async showToughts(req, res) {
-    res.render("toughts/home");
+    const toughtsData = await Tought.findAll({
+      include: User,
+    });
+    const toughts = toughtsData.map(result => result.get({plain: true}));
+    res.render("toughts/home", {toughts});
   }
 
   static async dashboard(req, res) {
@@ -76,7 +81,7 @@ module.exports = class ToughtController {
       const tought = {
         title: req.body.title,
       };
-      await Tought.updateOne(tought, { where: { id: id } });
+      await Tought.update(tought, { where: { id: id } });
       req.flash("message", "Pensamento atualizado com sucesso");
       req.session.save(() => {
         res.redirect("/toughts/dashboard");
